fd5fbf1bf6bec80222ce6792046e449c7d919252
hedi30 <hedihamida096@gmail.com>
2026-04-13 03:54:25 +0100

    treesiter config update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
--- Highlight, edit, and navigate code
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    'neovim-treesitter/treesitter-parser-registry',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').setup {}
+
+      require('nvim-treesitter').install {
         'lua',
         'python',
         'javascript',
@@ -32,15 +35,47 @@ return {
         'css',
         'html',
         'c',
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    }
+      }
 
-    vim.filetype.add { extension = { tf = 'terraform' } }
-    vim.filetype.add { extension = { tfvars = 'terraform' } }
-    vim.filetype.add { extension = { pipeline = 'groovy' } }
-    vim.filetype.add { extension = { multibranch = 'groovy' } }
-  end,
+      vim.filetype.add { extension = { tf = 'terraform' } }
+      vim.filetype.add { extension = { tfvars = 'terraform' } }
+      vim.filetype.add { extension = { pipeline = 'groovy' } }
+      vim.filetype.add { extension = { multibranch = 'groovy' } }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'lua',
+          'python',
+          'javascript',
+          'typescript',
+          'vim',
+          'terraform',
+          'sql',
+          'dockerfile',
+          'toml',
+          'json',
+          'java',
+          'groovy',
+          'go',
+          'gitignore',
+          'graphql',
+          'yaml',
+          'make',
+          'cmake',
+          'markdown',
+          'bash',
+          'tsx',
+          'css',
+          'html',
+          'c',
+        },
+        callback = function()
+          vim.treesitter.start()
+          pcall(function()
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end)
+        end,
+      })
+    end,
+  },
 }
